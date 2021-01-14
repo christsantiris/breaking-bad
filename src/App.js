4b4c98c3fe8795172edeefdd7177696aa7dd2106
Chris Tsantiris <chris.tsantiris@gmail.com>
2021-01-14 12:30:40 -0500

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Header from './components/ui/header'
+import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 import './App.css';
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchITems = async () => {
-      const result = await axios(`https://breakingbadapi.com/api/characters`)
+      const result = await axios(`https://breakingbadapi.com/api/characters?name=${query}`)
       setItems(result.data)
       setIsLoading(false)
     }
     fetchITems()
-  }, [])
+  }, [query])
 
   return (
     <div className="App">
       <Header />
-      <CharacterGrid isLoading={isLoading} items={items}/>
+      <Search getQuery={(query) => setQuery(query)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+import { getSuggestedQuery } from '@testing-library/react'
+import React, { useState } from 'react'
+
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('')
+    const onChange = (query) => {
+        setText(query)
+        getQuery(query)
+    }
+
+    return (
+        <section className='search'>
+            <form>
+                <input 
+                type='text' 
+                className='form-control' 
+                placeholder='Search characters' 
+                autoFocus  
+                value={text} 
+                onChange={(e) => onChange(e.target.value)}
+            />
+            </form>
+        </section>
+    )
+}
+
+export default Search

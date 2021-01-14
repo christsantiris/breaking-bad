0d792cdf3761c1e466eab316d234c3bd4d2aa3d9
Chris Tsantiris <chris.tsantiris@gmail.com>
2021-01-14 11:57:17 -0500

    Return character data from breaking bad api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/ui/header'
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchITems = async () => {
+      const result = await axios(`https://breakingbadapi.com/api/characters`)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchITems()
+  }, [])
+
   return (
     <div className="App">
       <Header />
